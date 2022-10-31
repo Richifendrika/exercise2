d1619bbfcbe0de6fcc58e52077395b0293607aa2
Richi Fendrika <ASUS@LAPTOP-18FMB87U>
2022-10-31 16:26:37 +0700

    change string to int

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace exercise2
 {
-    internal class Program
+    public class Program
     {
-        int[] arr = new int[30];
+        int[] Richi = new int[30];
+        int n;
+
+        public void read()
+        {
+            while (true)
+            {
+                Console.Write("Masukkan jumlah data yang akan diurutkan : ");
+                string s = Console.ReadLine();
+
+                n = Int32.Parse(s);
+                if (n > 0 && n <= 30)
+                    break;
+                else
+                    Console.WriteLine("\nJumlah maksimal data yang harus dimasukkan adalah 30. \n");
+            }
+        
+
         static void Main(string[] args)
         {
         }
